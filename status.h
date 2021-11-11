b66b81b803d970e4aed210f5fd7b3aaca4292154
James Butcher <jamesbutcher167@gmail.com>
2021-11-11 18:22:00 +0000

    Add all changes to dwm

---
+++ status.h
@@ -0,0 +1,9 @@
+char * smprintf(char *, ...);
+void settz(char *);
+char * mktimes(char *, char *);
+void setstatus(Display *, char *);
+char * readfile(char *, char *);
+char * getbattery(char *);
+void * statusloop(void *);
+void startstatusloop(Display *);
+
